4eb7c00083d6880b3556cbd922daf922c7ac65c5
jas9111 <80049977+jas9111@users.noreply.github.com>
2021-04-08 18:03:45 +0530

    Add files via upload

---
+++ c 21.js
@@ -0,0 +1,35 @@
+var bullet,wall;
+var speed,weight,thickness;
+
+function setup() 
+{
+createCanvas(1600,400);
+background("black");
+ 
+speed=random(223,321);
+weight=random(30,52);
+thickness=random(22,83);
+
+bullet=createSprite(50,200,50,5);
+wall=createSprite(1200,200,thickness,height/2);
+wall.shapeColor="gray";
+
+bullet.velocityX = speed;
+}
+
+function draw() {
+  background("white");  
+  bullet.debug=true;
+  wall.debug=true;
+
+  if(hasCollided(bullet,wall)){
+    bullet.velocityX=0;
+    var damage = 0.5*weight*speed*speed/22509;
+    if(damage>10){
+      wall.shapeColor="red";
+    }
+    if(damage<10){
+      wall.shapeColor="green";
+    }
+     
+    }
